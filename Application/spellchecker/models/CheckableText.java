05f91942b180658cca6593d0d273ec799cd874e4
z5191634 <z5191634@cse.unsw.edu.au>
2022-05-28 23:31:14 +1000

    add .gitignore

--- Application/spellchecker/models/CheckableText.java
+++
@@ -1,9 +0,0 @@
-package spellchecker.models;
-
-public abstract class CheckableText {
-  public String text;
-
-  public abstract boolean check(Database database);
-
-  public abstract int length();
-}
